081a7120fb045b632b5f8b6b2b421bd024101130
mishraanis <mishraanis@gmail.com>
2022-03-22 22:47:40 +0530

    commit 1

---
+++ pages/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div id="navigation-bar" className="flex absolute top-8 left-10 w-48 h-16 shadow-2xl rounded-full">
+      <div id="Potentio-logo" className="flex absolute w-40 h-9 top-6 left-10 font-comfortaa bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text text-2xl"> Potentio</div>
+    </div>
+  )
+}
