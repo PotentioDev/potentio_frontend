89bfbad334e12bb2998d41f1b213d2e5c1a4ede3
VisShon <vishnu20414@iiitd.ac.in>
2022-05-05 02:15:18 +0530

    yaya

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,7 @@ class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" rel="stylesheet"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet" />
