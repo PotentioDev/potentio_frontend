86a9e52db03f12889a9794bee1d0f2f43c1b9291
Heemank Verma <heemankv@gmail.com>
2022-05-05 01:50:51 +0530

    data

---
+++ data.js
@@ -0,0 +1,10 @@
+const Chemistry_Question = [
+  {
+    Question_Text: "What is the atomic number of Hydrogen?",
+    Options: ["1", "2", "3", "4"],
+    Answer: "1",
+    Tags: ["Chemistry", "Atomic Number"],
+    Level: 1,
+    Image: "",
+  },
+];
