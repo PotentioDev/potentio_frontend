b60e7134e808a181a66ee30b9d5b596a4e1afb4f
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-05-05 14:03:38 +0530

    bugfix

--- pages/testingZone.jsx
+++ pages/testingZone.jsx
@@ -588,8 +588,8 @@ export default function MainTestingZone() {
 </g>
 <path d="M35.482 42C35.002 42 34.762 41.7653 34.762 41.296C34.762 40.8373 35.002 40.608 35.482 40.608H37.434V32.624L35.93 33.584C35.7273 33.712 35.5353 33.7493 35.354 33.696C35.1833 33.632 35.05 33.52 34.954 33.36C34.8686 33.2 34.842 33.0293 34.874 32.848C34.9166 32.656 35.0393 32.496 35.242 32.368L37.45 30.976C37.6313 30.8693 37.802 30.784 37.962 30.72C38.1326 30.6453 38.2926 30.608 38.442 30.608C38.6233 30.608 38.7726 30.6613 38.89 30.768C39.018 30.8747 39.082 31.04 39.082 31.264V40.608H40.874C41.3646 40.608 41.61 40.8373 41.61 41.296C41.61 41.7653 41.3646 42 40.874 42H35.482Z" fill="black"/>
 <defs>
-<filter id="filter0_dd_1244_2272" x="0.798828" y="0" width="82" height="82" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<filter id="filter0_dd_1244_2272" x="0.798828" y="0" width="82" height="82" filterUnits="userSpaceOnUse">
+<feFlood result="BackgroundImageFix"/>
 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
 <feOffset dx="4" dy="4"/>
 <feGaussianBlur stdDeviation="7.5"/>
