20873dabe992d073bc686e420c8163f1e378ceb4
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-05-05 21:37:37 +0530

    new

--- pages/leaderboard.jsx
+++ pages/leaderboard.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
-import { Profiles } from "../data";
 function Leaderboard() {
   const [data, setData] = useState({
     profiles: [],
